cf0ab88bba4aae491c000aa9c3767ce5e35de886
Magnus Muhr <magnus.muhr.6587@student.uu.se>
2014-12-11 17:24:00 +0100

    moved resep

---
+++ resep/network.h
@@ -0,0 +1,10 @@
+struct neighbor {};
+
+struct node {};
+
+
+char* shortestPath(char *h1, char *h2, int traveltime, int buschanges, int line);
+
+void connectNodes(int line, struct node *node1, struct node *node2, int time);
+
+int uniqueNames();
