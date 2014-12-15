22545755d8f1305dde271aef381c513c6510beaf
Staffan <staffanedstrom@gmail.com>
2014-12-14 16:39:32 -0800

    forsta klar

--- resep/net.c
+++ resep/net.c
@@ -18,16 +18,19 @@ typedef struct node {
   void* next;
 } node;
 
-node *root;
+node* root;
+node* visited;
 FILE* fp;
 char inputFileName[] = "nat.txt";
 char c;
 
 node* getLastNode(node* start) {
   node* tempNode = start;
+  //printf("den sista noden: %s\n", tempNode->name);
+  //printf("den sista pekaren: %p\n", tempNode->next);
   while(tempNode->next != NULL) {
     tempNode = tempNode->next;
-  }
+  }     
   return tempNode;
 }
 
@@ -41,29 +44,37 @@ edgedata* getLastEdge(edgedata* start) {
 
 /**
    Creates a new node with a given name
-   Sets first node created to root
+   Sets first node created to root list
    Links the node with previously added nodes
    @param name name of the node
    @return address to the new node
 */
-node* newNode(char* name) {
+node* newNode(node** list, char* name) {
+
+  char* finalName = calloc(sizeof(char)*strlen(name), 1);
+  strcpy(finalName, name);
+          
   node* nodePointer = malloc(sizeof(node));
   //ibland kommer det ngt konstigt med name?! fixar det har
-  int length = strlen(name) - 1;
+  int length = strlen(finalName) - 1;
   while(length != 0) {
-    if(!isalpha(name[length]) && name[length] != ' ') {
-      //printf("en fuling hittad: %c - på plats %d\n", name[length], length);
-      name[length] = '\0';
+    if(!isalpha(finalName[length]) && finalName[length] != ' ') {
+      //printf("en fuling hittad: %c - på plats %d\n", finalName[length], length);
+      finalName[length] = '\0';
     }
     length--;
   }
+ 
+  nodePointer->name = finalName;
   
-  nodePointer->name = name;
-  if(root == NULL) {
-    root = nodePointer;
+  if(*list == NULL) {
+    //puts("detta blir roten i listan\n");
+    *list = nodePointer;
   } else {
-    getLastNode(root)->next = nodePointer;
+    getLastNode(*list)->next = nodePointer;
   }
+  nodePointer->next = NULL;
+  //printf("nyligen addad!: %s\n", getLastNode(*list)->name);
   return nodePointer;
 }
 
@@ -71,8 +82,9 @@ node* newNode(char* name) {
 
 
 
-node* findNode(node* root, char* name) {
-  node* tempNode = root;
+node* findNode(node* list, char* name) {
+  node* tempNode = list;
+
   while(tempNode != NULL) {
     // printf("jfr %s = %s\n", name, tempNode->name);
     if(!strcmp(name, tempNode->name)) {
@@ -81,19 +93,20 @@ node* findNode(node* root, char* name) {
     tempNode = tempNode->next;
   }
   
+  
   return NULL;
 }
 
 
-void removeNode(char* name) {
-  node* nodetokill = findNode(root, name);
-  if(nodetokill == root) {
+void removeNode(node* list, char* name) {
+  node* nodetokill = findNode(list, name);
+  if(nodetokill == list) {
     //if node is first
-    root = nodetokill->next;
+    list = nodetokill->next;
     free(nodetokill);
   } else if(nodetokill != NULL) {
     //find previous node
-    node* prevNode = root;
+    node* prevNode = list;
     while(prevNode->next != nodetokill) {
       prevNode = prevNode->next;
     }
@@ -183,21 +196,19 @@ void initBusStops() {
       if(step == 2 || step == 3) {
 
 
-    
         if(findNode(root, tempName) == NULL) {
 
           //Try to add node
-          char* finalName = calloc(sizeof(char)*strlen(tempName), 1);
-          strcpy(finalName, tempName);
-          
+
           switch(step) 
             {
-            case(2): step2 = newNode(finalName); break;
-            case(3): step3 = newNode(finalName); 
+            case(2): step2 = newNode(&root, tempName); break;
+            case(3): step3 = newNode(&root, tempName); 
               //printf("step3 add!: %s\n", finalName);
-              //printf("nyligen addad!: %s\n", getLastNode(root)->name);
+              
               break;
             }
+        
           //  printf("addad nod: %s\n", getLastNode(root)->name);
         } else {
           // printf("%s redan addad!\n", tempName);
@@ -235,42 +246,139 @@ void initBusStops() {
   fclose(fp);
 }
 
-void shortestPath(node* root, char* from, char* to) {
+void possiblePaths(char* from, char* to) {
   //kolla om de finns som edge och pa samma linje
   //i varje edge, kör hela funktionen
   //kolla om de är på samma busslinje
+  char* tempName = malloc(sizeof(char)*50);//name of bus stop
   node* fromNode = findNode(root, from);
   node* toNode = findNode(root, to);
   assert(fromNode != NULL);
   assert(toNode != NULL);
-
   
-  puts("starts search...");
+  edgedata* tempEdge = fromNode->edge;
+    strcpy(tempName, fromNode->name); 
+
+    while(tempEdge != NULL) {
+      node* visitedList = NULL;
+      newNode(&visitedList, fromNode->name);
+      node* edgeNode = ((node*) tempEdge->node);
+      //   printf("visited list star pa: %p\n", visitedList);
+    /*if(fromNode == toNode) {
+      //printa path o sdnt
+      printf("found it efter ett steg");
+      return;
+      }*/
+    
+      node* secondNode = newNode(&visitedList, edgeNode->name);
+      // printf("visited list nxt star pa: %p\n", visitedList->next);
+      searchPath(visitedList, secondNode, toNode, tempEdge->line);
+      
+    tempEdge = tempEdge->next;
+  }
+  free(tempName);
+  //puts("starts search...");
 }
 
 
-void printAllStops() {
+void printNodeList(int viewConnections, node* list) {
   puts("_alla stop_");
-  node* tempNode = root;
+  node* tempNode = list;
+  if(tempNode == NULL) {
+    puts("omg! no stops added!");
+  }
   while(tempNode != NULL) {
+    // puts("this name crashes it!");
+    // printf("this name crashes it! %s \n", tempNode->name);
     char* thename = tempNode->name;
-    printf("adr: %p, name: %s\n", tempNode, thename);
-    edgedata* tempEdge = tempNode->edge;
-    int i = 1;
-    while(tempEdge != NULL) {
-  
-      printf("%d >%s\n", i, ((node*)(tempEdge->node))->name);
-      tempEdge = tempEdge->next;
-      i++;
-      
+    printf("adr: %p, nxt: %p, name: %s\n", tempNode, tempNode->next, thename);
+    if(viewConnections == 1) {
+      edgedata* tempEdge = tempNode->edge;
+      int i = 1;
+      while(tempEdge != NULL) {
+        
+        printf("%d >%s line: %d\n", i, ((node*)(tempEdge->node))->name, tempEdge->line);
+        tempEdge = tempEdge->next;
+        i++;
+        
+      }
     }
     tempNode = tempNode->next;
   }
 }
 
+void printTravelPath(node* list, int busline) {
+  printf("\n -- Possible path by bus %d:\n", busline);
+  node* tempNode = list;
+  int totaltime = 0;
+  int time = 0;
+  while(tempNode != NULL) {
+    //get time from here to next
+    node* rootNode = findNode(root, tempNode->name);
+    edgedata* tempEdge = rootNode->edge;
+
+    if(tempNode->next != NULL) {
+      while(tempEdge != NULL) {
+
+        node* nextRootNode = findNode(root, ((node*)(tempNode->next))->name);
+        if(tempEdge->node == nextRootNode) {
+          time = tempEdge->time;
+          totaltime += time;
+        }
+        tempEdge = tempEdge->next;
+      }
+
+      printf("%s \t %d mins\n", tempNode->name, time);
+    } else {
+      printf("%s\n", tempNode->name);
+    }
+    tempNode = tempNode->next;
+  }
+  puts("--------------------");
+  printf("total time: %d mins\n", totaltime);
+ 
+}
+
+void searchPath(node* visitedList, node* current, node* to, int busline) {
+  edgedata* tempEdge = findNode(root, current->name)->edge;
+  char* tempName = malloc(sizeof(char)*50);//name of bus stop
+  // printf("riding from %p %s, on line %d\n", visitedList, current->name, busline);
+  while(tempEdge != NULL) {
+    node* edgeNode = tempEdge->node;
+    if(findNode(visitedList, edgeNode->name) == NULL) {
+      //printf("en mojilg resvag: %s, %p\n", edgeNode->name, findNode(visitedList, edgeNode->name));
+
+      //kontrollera om samma busslinje
+      if(tempEdge->line == busline) {
+        //nod kopplad till denna edge
+        node* edgeNode = tempEdge->node;    
+        if(edgeNode == to) {
+          strcpy(tempName, edgeNode->name);
+          newNode(&visitedList, tempName);
+          // printf("reached goal %p %s Bus line: %d!\n", to, to->name, busline);
+          //printa hela listan da
+          printTravelPath(visitedList, busline);
+          //printNodeList(0, visitedList);
+        } else {
+          strcpy(tempName, edgeNode->name);
+          node* searchThisNode = newNode(&visitedList, tempName);
+
+          searchPath(visitedList, searchThisNode, to, tempEdge->line);
+        }
+      }
+    }
+    
+    tempEdge = tempEdge->next;
+   }
+  //printNodeList(0, visitedList);
+  //free(visitedList);
+}
+
+
 int main()
 {
-  initBusStops();
+   initBusStops();
+
 
   // removeNode("Centralstationen");
   // removeNode("Kalle Anka");
@@ -279,7 +387,24 @@ int main()
   //  node* testrm = findNode(root, "Langvagen");
   //removeEdge(testrm, "Gottsundavagen");
   // removeEdge(testrm, "Cellovagen");
-  printAllStops();
+ 
+  
+  printNodeList(1, root);
+ 
+  possiblePaths("Centralstationen", "Cellovagen");
+  
+  
+  
+  
+ 
+  // printNodeList(0, visited);
+  
+/*node* tempNode = visited;
+  while(tempNode->next != NULL) {
+    puts("hittade en nod\n");
+    tempNode = tempNode->next;
+    }*/
+
 
   // newNode("Biomedicinskt centrum");
   //printf("%p %p\n", findNode(root, "Biomedicinskt centrum"), findNode(root, "Akademiska sodra"));
